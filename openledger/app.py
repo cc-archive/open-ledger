0f0696d32057e3e4189df03144ef539dafe72816
Liza Daly <lizadaly@gmail.com>
2016-09-15 09:26:30 -0400

    Add runner for development

--- openledger/app.py
+++ openledger/app.py
@@ -8,3 +8,7 @@ def index():
     from handlers.handler_500px import photos
     results = photos(search='greyhound')
     return render_template('index.html', results=results)
+
+if __name__ == '__main__':
+    # Run me as 'python -m app' to avoid path/import problems
+    app.run(debug=True)
