a77ce4d076e10f8b8d8ad157a9c083e38289e4f9
Liza Daly <lizadaly@gmail.com>
2016-09-15 11:17:35 -0400

    Integrate simple search form

--- openledger/app.py
+++ openledger/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import forms
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
@@ -6,9 +8,14 @@ app.config.from_pyfile('config.py')
 @app.route("/")
 def index():
     from handlers.handler_500px import photos
-    results = photos(search='greyhound')
-    return render_template('index.html', results=results)
+    search = request.args.get('search')
+    form = forms.SearchForm()
+    if search:
+        results = photos(search=search)
+    else:
+        results = None
+    return render_template('index.html', results=results, form=form, search=search)
 
 if __name__ == '__main__':
     # Run me as 'python -m app' to avoid path/import problems
-    app.run(debug=True)
+    app.run()
