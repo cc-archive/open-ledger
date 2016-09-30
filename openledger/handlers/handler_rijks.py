3146a93f319a1c1df70e091db04b4b8f586cff4c
Liza Daly <lizadaly@gmail.com>
2016-09-30 10:44:56 -0400

    [#2] Rijks pagination

--- openledger/handlers/handler_rijks.py
+++ openledger/handlers/handler_rijks.py
@@ -6,19 +6,25 @@ BASE_URL = 'https://www.rijksmuseum.nl'
 ENDPOINT_PHOTOS = BASE_URL + '/api/en/collection'
 
 def photos(search=None, page=1, per_page=20, **kwargs):
+    # Rijks pages are zero-indexed, so always subtract one before the request
+    p = int(page) - 1
     params = {
         'format': 'JSON',
         'q': search,
         'key': app.config['API_RIJKS'],
         'imgonly': 'True',  # This doesn't seem to have an effect, so filter
         'culture': 'en',
-        'ps': per_page * 2,  # Results per page
+        'p': p, # Page number
+        'ps': per_page,  # Results per page
     }
     r = requests.get(ENDPOINT_PHOTOS, params=params)
     results = r.json()
 
-    # Since the imgonly parameter doesn't work, filter ourselves and cut down the
-    # result set
+    # Since the imgonly parameter doesn't work, filter ourselves and cut down the result set
     filtered = [res for res in results.get('artObjects') if res.get('webImage')]
     results['artObjects'] = filtered[:per_page]
+
+    # Rijks doesn't give us a nice set of pages, so do that ourselves
+    results['page'] = int(page)
+    results['pages'] = int(results['count']) / per_page
     return results
