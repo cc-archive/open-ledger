2c9a0ba562e452c1b6d91bf8286bebdda895bfd8
Liza Daly <lizadaly@gmail.com>
2016-10-14 16:20:16 -0400

    [#34] Refinements for loader setup

--- fabfile.py
+++ fabfile.py
@@ -4,8 +4,10 @@ import boto3
 import botocore
 import logging
 
+import fabtools
 from fabric.api import local, settings, abort, run, cd, env, put, sudo, hosts
 from fabric.contrib.console import confirm
+from fabric.exceptions import NetworkError
 
 timestamp="release-%s" % int(time.time() * 1000)
 
@@ -14,13 +16,23 @@ CURRENT_BRANCH = 'master'
 
 DEBUG = False
 
-AMI = 'ami-27612947'
 TAG = 'open-ledger-loader'
+AMI = os.environ['OPEN_LEDGER_LOADER_AMI']
+KEY_NAME = os.environ['OPEN_LEDGER_LOADER_KEY_NAME']
+SECURITY_GROUP = os.environ['OPEN_LEDGER_LOADER_SECURITY_GROUP']
+
+# INSTANCE_TYPE = 'r3.large'
+INSTANCE_TYPE = 't2.micro'
 
 console = logging.StreamHandler()
 log = logging.getLogger(__name__)
 log.addHandler(console)
 log.setLevel(logging.DEBUG)
+
+log.debug("Starting up with ami=%s, key=%s, security=%s", AMI, KEY_NAME, SECURITY_GROUP)
+
+class LoaderException(Exception):
+    pass
 
 def launchloader():
     """Launch an EC2 instance with the loader"""
@@ -28,7 +40,10 @@ def launchloader():
     resource, client = _init_ec2()
     try:
         instance = _get_running_instance(resource, client)
-        print(instance.public_ip_address)
+        deploy_code(instance.public_ip_address)
+    except LoaderException as e:
+        log.exception(e)
+        instance.stop()
     except:
         raise
     finally:
@@ -36,8 +51,49 @@ def launchloader():
         #instance.stop()
         pass
 
+def deploy_code(host_string):
+    max_retries = 5
+    retries = 0
+    with settings(host_string="ec2-user@" + host_string):
+        while True:
+            try:
+                fabtools.require.git.working_copy('https://github.com/creativecommons/open-ledger.git')
+                with cd('open-ledger'):
+                    run('virtualenv venv --python=python3')
+                    run('./venv/bin/pip install -r loader-requirements.txt -q')
+                    retries = 6
+            except NetworkError:
+                time.sleep(5)
+                retries += 1
+            if retries > max_retries:
+                raise LoaderException("Timed out waiting for ssh")
+
+def stop_loaders():
+    """Terminate all loading instances that are running"""
+    instance_ids, resource = _get_running_instances()
+    [resource.Instance(i).stop() for i in instance_ids]
+    log.info("Stopped instances %s", ", ".join(instance_ids)) if len(instance_ids) > 0 else None
+
+def terminate_loaders():
+    """Terminate all loading instances that are running"""
+    instance_ids, resource = _get_running_instances()
+    [resource.Instance(i).terminate() for i in instance_ids]
+    log.info("Terminated instances %s", ", ".join(instance_ids)) if len(instance_ids) > 0 else None
+
+def _get_running_instances():
+    resource, client = _init_ec2()
+    resp = client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [TAG]},
+                                              {'Name': 'instance-state-name', 'Values': ['running']}])
+    instance_ids = []
+    for r in resp['Reservations']:
+        for i in r['Instances']:
+            instance_ids.append(i['InstanceId'])
+    if len(instance_ids) == 0:
+        log.info("No running instances found")
+    return instance_ids, resource
+
+
 def _get_running_instance(resource, client):
-    instance_type = 'r3.large'
     resp = client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [TAG]},
                                               {'Name': 'instance-state-name', 'Values': ['running']}])
     instance = None
@@ -54,13 +110,21 @@ def _get_running_instance(resource, client):
                                               {'Name': 'instance-state-name', 'Values': ['stopped']}])
         for r in resp['Reservations']:
             for i in r['Instances']:
-                log.debug("Starting previously stopped instance")
-                instance = i
+                instance = resource.Instance(i['InstanceId'])
+                log.debug("Starting previously stopped instance %s", i['InstanceId'])
                 instance.start()
                 break
+            if instance:
+                break
+
         if not instance:
             log.debug("No stopped instances found; starting a brand new one...")
-            instance = resource.create_instances(MinCount=1, MaxCount=1, InstanceType=instance_type, ImageId=AMI)[0]
+            instance = resource.create_instances(MinCount=1, MaxCount=1,
+                                                 SecurityGroups=[SECURITY_GROUP],
+                                                 KeyName=KEY_NAME,
+                                                 InstanceType=INSTANCE_TYPE,
+                                                 UserData=user_data,
+                                                 ImageId=AMI)[0]
             instance.wait_until_running()
             log.debug("Adding tag %s", TAG)
             instance.create_tags(Tags=[{'Key': 'Name', 'Value': TAG}])
@@ -77,10 +141,27 @@ def _init_ec2():
 def deploy():
     with cd(CODE_DIR):
         run('git pull origin ' + CURRENT_BRANCH)
-        run('./venv/bin/pip install -r requirements.txt')
+        run('./venv/bin/pip install -r requirements.txt -q')
         run('npm install')
         run('NODE_ENV=production node_modules/.bin/webpack')
     restart_host()
 
 def restart_host():
     sudo('service openledger restart', shell=False)
+
+
+user_data = """
+#cloud-config
+repo_update: true
+repo_upgrade: all
+
+packages:
+ - gcc
+ - nodejs
+ - git
+ - python34
+ - python34-virtualenv
+ - postgresql93
+ - postgresql93-devel
+ - libjpeg-turbo-devel
+"""
