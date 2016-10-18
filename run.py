8f454419967eef331a2a144fa11b9da3c59d7025
Liza Daly <lizadaly@gmail.com>
2016-10-18 09:02:16 -0400

    Move debugger to dev run script

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from openledger import application
+from flask_debugtoolbar import DebugToolbarExtension
 
 if __name__ == '__main__':
     # Run me in development as 'python -m app' to avoid path/import problems
+    toolbar = DebugToolbarExtension(application)
     application.run(debug=True)
