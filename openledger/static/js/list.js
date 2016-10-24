c05171fe12ac3fb4adf49fce1cc6f2095737fc76
Liza Daly <lizadaly@gmail.com>
2016-10-24 17:36:55 -0400

    [#21] Make add-to-list more professional

--- openledger/static/js/list.js
+++ openledger/static/js/list.js
@@ -4,35 +4,60 @@ const API_BASE = '/api/v1/'
 
 // Add an item by identifier to a list. If the list doesn't exist, create it
 export const addToList = (identifier, list) => {
-
+  // TODO
 }
 
 export const addToListForm = (e) => {
   // Bring up a form to capture a list title from a user
   e.preventDefault()
   var form = e.target.nextElementSibling
+  var input = form.querySelector('input[type=text]')
+
+  // Clear the old message if it's still there
+  var msg = form.nextElementSibling
+  msg.style.display = 'none'
+
   form.style.display = 'block'
+  form.classList.toggle('pulse')
   form.addEventListener('submit', createList)
 
+  input.focus()
+  input.scrollIntoView()
 }
 
 
 // Create a list, returning the list slug
 export const createList = (e) => {
-  e.preventDefault()
   const url = API_BASE + 'lists'
-  var form = new FormData(e.target)
+
+  e.preventDefault()
+
+  var data = new FormData(e.target)
+  var form = e.target
+
+  form.style.display = 'none'
+
+  setTimeout(() => {
+    form.classList.toggle('pulse')
+    form.classList.toggle('bounceOut')
+    form.reset()
+  }, 1000)
+
+  var msg = e.target.nextElementSibling
+  msg.style.display = 'block'
+  msg.innerHTML = "Saving..."
 
   fetch(url, {
     method: 'POST',
-    body: form
+    body: data
   })
   .then((response) => {
     return response.json()
   })
   .then((json) => {
     if (json.hasOwnProperty('slug')) {
-      console.log(`Created list "${form.get('title')}" with slug ${json.slug} and image ${form.get('identifier')}`)
+      console.log(`Created list "${data.get('title')}" with slug ${json.slug} and image ${data.get('identifier')}`)
+      msg.innerHTML = `Your image was saved to <a href="/lists/${json.slug}">${data.get('title')}</a>`
     }
     else {
       console.log(json)
