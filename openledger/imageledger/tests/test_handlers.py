adfc6671244fe27e4036f582cbbd8d225a64d399
Liza Daly <lizadaly@gmail.com>
2016-11-11 10:20:29 -0500

    Provider tests

---
+++ openledger/imageledger/tests/test_handlers.py
@@ -0,0 +1,156 @@
+import unittest
+import responses
+import requests
+
+from imageledger.handlers.handler_500px import photos as search_500
+from imageledger.handlers.handler_rijks import photos as search_rijks
+from imageledger.handlers.handler_flickr import photos as search_flickr
+from imageledger.handlers.handler_wikimedia import photos as search_wikimedia
+
+from imageledger.tests.utils import *
+
+class FlickrTestCase(unittest.TestCase):
+    def setUp(self):
+        activate_flickr_mocks()
+
+    @responses.activate
+    def test_flickr_search(self):
+        """The Flickr handler should return a FlickrAPI object when authenticating"""
+        resp = search_flickr(search="test")
+        assert resp
+
+    @responses.activate
+    def test_flickr_response_length(self):
+        """The Flickr handler should return the expected number of responses"""
+        resp = search_flickr(search="test")
+        assert 20 == len(resp['photos']['photo'])
+
+    @responses.activate
+    def test_flickr_page(self):
+        """The Flickr handler should return the current page of the result set"""
+        resp = search_flickr(search="test")
+        assert 1 == resp['photos']['page']
+
+    @responses.activate
+    def test_flickr_total_pages(self):
+        """The Flickr handler should return the total number of pages of the result set"""
+        resp = search_flickr(search="test")
+        assert 739 == resp['photos']['pages']  # For some reason Flickr gives this as a string, sigh
+
+    @responses.activate
+    def test_flickr_total_results(self):
+        """The Flickr handler should return the total number of results"""
+        resp = search_flickr(search="test")
+        assert 14771 == resp['photos']['total']  # For some reason Flickr gives this as a string, sigh
+
+
+class FiveHundredPixelsTestCase(unittest.TestCase):
+    def setUp(self):
+        activate_500px_mocks()
+
+    @responses.activate
+    def test_500px_photos(self):
+        """The 500px handler should return a 500px API object when authenticating"""
+        resp = search_500(search="test")
+        assert resp
+
+    @responses.activate
+    def test_500px_response_length(self):
+        """The 500px handler should return the expected number of responses"""
+        resp = search_500(search="test")
+        assert 20 == len(resp['photos'])
+
+    @responses.activate
+    def test_500px_page(self):
+        """The 500px handler should return the current page of the result set"""
+        resp = search_500(search="test")
+        assert 1 == resp['current_page']
+
+    @responses.activate
+    def test_500px_total_pages(self):
+        """The 500px handler should return the total number of pages of the result set"""
+        resp = search_500(search="test")
+        assert resp['total_pages'] > 1
+
+    @responses.activate
+    def test_500px_total_results(self):
+        """The 500px handler should return the total number of results"""
+        resp = search_500(search="test")
+        assert resp['total_items'] > 1
+
+
+class RijksTestCase(unittest.TestCase):
+    def setUp(self):
+        activate_rijks_mocks()
+
+    @responses.activate
+    def test_rijks_search(self):
+        """The rijks handler should return a rijks API object when authenticating"""
+        resp = search_rijks(search="test")
+        assert resp
+
+    @responses.activate
+    def test_rijks_response_length(self):
+        """The rijks handler should return the expected number of responses"""
+        resp = search_rijks(search="test")
+        assert 20 == len(resp['artObjects'])
+
+    @responses.activate
+    def test_rijks_page(self):
+        """The rijks handler should return the current page of the result set"""
+        resp = search_rijks(search="test")
+        assert 1 == resp['page']
+
+    @responses.activate
+    def test_rijks_total_pages(self):
+        """The rijks handler should return the total number of pages of the result set"""
+        resp = search_rijks(search="test")
+        assert 274 == resp['pages']
+
+    @responses.activate
+    def test_rijks_total_results(self):
+        """The rijks handler should return the total number of results"""
+        resp = search_rijks(search="test")
+        assert 5489 == resp['count']
+
+class WikimediaTestCase(unittest.TestCase):
+    def setUp(self):
+        activate_wikimedia_mocks()
+
+    @responses.activate
+    def test_wikimedia_entity(self):
+        """The wikimedia handler should return an entity response to a text query"""
+        from imageledger.handlers.handler_wikimedia import entity_search
+        resp = entity_search(search="test")
+        assert 1 == len(resp.get('search'))
+
+    @responses.activate
+    def test_wikimedia_response_length(self):
+        """The wikimedia handler should return the expected number of responses"""
+        resp = search_wikimedia(search="test")
+        assert 9 == len(resp['results'])
+
+    @responses.activate
+    def test_wikimedia_page(self):
+        """The wikimedia handler should return the current page of the result set"""
+        resp = search_wikimedia(search="test")
+        assert 1 == resp['page']
+
+    @responses.activate
+    def test_wikimedia_total_pages(self):
+        """The wikimedia handler should return the total number of pages of the result set"""
+        resp = search_wikimedia(search="test")
+        assert 1 == resp['pages']
+
+    @responses.activate
+    def test_wikimedia_total_results(self):
+        """The wikimedia handler should return the total number of results"""
+        resp = search_wikimedia(search="test")
+        assert 9 == resp['total']
+
+    def test_wikimedia_sparql(self):
+        """The SPARQL construction method should return a valid SPARQL statement"""
+        from rdflib.plugins.sparql import prepareQuery
+        from imageledger.handlers.handler_wikimedia import prepare_sparql_query
+        q = prepareQuery(prepare_sparql_query("W30", 100))
+        assert q  # This should parse successfully
---
+++ openledger/imageledger/tests/utils.py
@@ -0,0 +1,68 @@
+import json
+import os
+
+import jinja2
+from django.test import TestCase
+import html5lib
+from lxml.html import tostring, html5parser
+import responses
+
+from imageledger import models
+#from imageledger.views import auth_views
+
+TESTING_CONFIG = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.py')
+
+class TestimageledgerApp(TestCase):
+    """Setup/teardown for app test cases"""
+
+    pass
+
+def activate_all_provider_mocks():
+    """Mock all the responses we know about from all the providers"""
+    activate_500px_mocks()
+    activate_rijks_mocks()
+    activate_flickr_mocks()
+    activate_wikimedia_mocks()
+
+def activate_flickr_mocks():
+    from imageledger.handlers.handler_flickr import auth
+    flickr = auth()
+    responses.add(responses.POST, flickr.REST_URL, status=200, content_type='application/json',
+                      json=load_json_data('flickr-response.json'))
+
+def activate_500px_mocks():
+    from imageledger.handlers.handler_500px import ENDPOINT_PHOTOS
+    responses.add(responses.GET, ENDPOINT_PHOTOS, status=200, content_type='application/json',
+                  json=load_json_data('500px-response.json'))
+
+def activate_rijks_mocks():
+    from imageledger.handlers.handler_rijks import ENDPOINT_PHOTOS
+    responses.add(responses.GET, ENDPOINT_PHOTOS, status=200, content_type='application/json',
+                  json=load_json_data('rijks-response.json'))
+
+def activate_wikimedia_mocks():
+    from imageledger.handlers.handler_wikimedia import BASE_URL, WIKIDATA_URL
+    entities_template = load_json_data('wikimedia-entities-response.json')
+    wikidata_template = load_json_data('wikimedia-data-response.json')
+    responses.add(responses.GET, BASE_URL, status=200, content_type='application/json',
+                  json=entities_template)
+    responses.add(responses.GET, WIKIDATA_URL, status=200, content_type='application/json',
+                  json=wikidata_template)
+
+def load_json_data(datafile):
+    """Load testing data in JSON format relative to the path where the test lives"""
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return json.loads(open(os.path.join(dir_path, datafile)).read())
+
+def select_node(rv, selector):
+    """Give a response from Flask, return just the HTML fragment defined by `selector`.
+    Guaranteed to return one node or an empty set."""
+    r = select_nodes(rv, selector)
+    if r and len(r) > 0:
+        return r[0]
+    return ()
+
+def select_nodes(rv, selector):
+    """Give a response from Flask, return just the HTML fragment defined by `selector`"""
+    h = html5lib.parse(rv.data.decode('utf-8'), treebuilder='lxml', namespaceHTMLElements=False)
+    return h.getroot().cssselect(selector)
