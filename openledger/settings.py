20994af4f855efb8527f5f0b9b3a6b8848b1e2bd
Liza Daly <lizadaly@gmail.com>
2016-11-17 13:03:08 -0500

    Add static file mechanism

--- openledger/settings.py
+++ openledger/settings.py
@@ -140,6 +140,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, "deploy")
 
 CACHES = {
