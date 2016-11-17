e2c5285b1b8413218a5b44275588065e8697fe51
Liza Daly <lizadaly@gmail.com>
2016-11-17 17:30:25 -0500

    Run tests with alternate settings for staticfiles

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openledger.settings")
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openledger.test_settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openledger.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
