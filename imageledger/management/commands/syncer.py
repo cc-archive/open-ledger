379d2df1041605d3c8a21d543f9955601ee07558
Liza Daly <lizadaly@gmail.com>
2016-11-29 14:26:52 -0500

    Add threading to syncer

--- imageledger/management/commands/syncer.py
+++ imageledger/management/commands/syncer.py
@@ -1,12 +1,14 @@
 from collections import namedtuple
 import itertools
 import logging
+from multiprocessing.dummy import Pool as ThreadPool
 
 from elasticsearch import helpers
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection, transaction
 
 from imageledger import models, search
+
 
 console = logging.StreamHandler()
 log = logging.getLogger(__name__)
@@ -44,11 +46,17 @@ class Command(BaseCommand):
             log.setLevel(logging.DEBUG)
         self.sync_all_images(chunk_size=options['chunk_size'], with_fingerprinting=options['with_fingerprinting'])
 
-    def sync_all_images(self, chunk_size=DEFAULT_CHUNK_SIZE, with_fingerprinting=False, num_iterations=1000):
+    def sync_all_images(self, chunk_size=DEFAULT_CHUNK_SIZE, with_fingerprinting=False, num_iterations=5):
         """Sync all of the images, sorting from least-recently-synced"""
-        count = 0
-        while count < num_iterations:
-            imgs = models.Image.objects.all().order_by('-last_synced_with_source')[0:chunk_size]
-            for img in imgs:
-                img.sync(attempt_perceptual_hash=with_fingerprinting)
-                count += 1
+        pool = ThreadPool(4)
+        starts = [i * chunk_size for i in range(0, num_iterations)]
+        pool.starmap(do_sync, zip(starts, itertools.repeat(chunk_size, num_iterations), itertools.repeat(with_fingerprinting, num_iterations)))
+        pool.close()
+        pool.join()
+
+def do_sync(start, chunk_size, with_fingerprinting):
+    end = start + chunk_size
+    log.info("Starting sync in range from %d to %d...", start, end)
+    imgs = models.Image.objects.all().order_by('-last_synced_with_source')[start:end]
+    for img in imgs:
+        img.sync(attempt_perceptual_hash=with_fingerprinting)
