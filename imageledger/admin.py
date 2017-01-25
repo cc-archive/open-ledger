acd4704321e232a3dc530bbf214f0ef2e29931b3
Liza Daly <lizadaly@gmail.com>
2017-01-25 14:47:29 -0500

    [#148] Enhancements for list admin

--- imageledger/admin.py
+++ imageledger/admin.py
@@ -7,15 +7,18 @@ from django.utils.safestring import mark_safe
 
 
 class ImageAdmin(admin.ModelAdmin):
-    fields = ( 'image_tag', 'title', 'provider', 'license', 'license_version')
-    readonly_fields = ('image_tag',)
-
-class ImageInline(admin.TabularInline):
-    model = models.List.images.through
+    list_display = ('title', 'creator', 'provider', 'created_on', 'last_synced_with_source')
+    fields = ( 'image_tag', 'title', 'provider', 'license', 'license_version', 'created_on', 'last_synced_with_source',
+              'foreign_landing_url', 'foreign_identifier')
+    readonly_fields = ('image_tag', 'created_on', 'last_synced_with_source', )
 
 class ListAdmin(admin.ModelAdmin):
-    inlines = [ ImageInline, ]
-    exclude = ('images',)
+    empty_value_display = '-blank-'
+    list_display = ('title', 'owner', 'num_images', 'is_public', 'created_on', 'updated_on')
+    readonly_fields = ('images', 'slug')
+    def num_images(self, obj):
+        return obj.images.all().count()
 
+#    filter_horizontal = ['images']
 admin.site.register(models.List, ListAdmin)
 admin.site.register(models.Image, ImageAdmin)
