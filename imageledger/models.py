5c184a8ce21adf8998d9bf42ef19b767e42114df
Paola Villarreal <paw@paw.mx>
2017-11-20 14:24:57 -0600

    Added __iter__ to the Image model so it can be used to display search results with metadata on lists and favorites along with search results.

--- imageledger/models.py
+++ imageledger/models.py
@@ -15,6 +15,14 @@ class OpenLedgerModel(models.Model):
 
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
+    def __iter__(self):
+        for field_name in self._meta.get_fields ():
+            try:
+                value = getattr (self, field_name, None)
+                yield (field_name, value)
+            except:
+                pass
 
     class Meta:
         abstract = True
