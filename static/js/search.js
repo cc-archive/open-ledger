7dcc776dd0b4b0d5b8d1cb2654ffc20b0cfdc82a
Paola Villarreal <paw@paw.mx>
2017-11-21 16:17:13 -0600

    Moved away from the 'advanced' variable in the request query and implemented javascript code to show or hide the advanced search filters. Also fixes #194 because we are only hidding the input elements so they still get sent when user changes the search terms.

--- static/js/search.js
+++ static/js/search.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   var workTypes = document.querySelectorAll('.work-type')
   for (let workType of workTypes) {
     let typeSelector = workType.querySelector('input[name="work_types"]')
@@ -12,4 +11,18 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
   }
+    var adv_search = document.getElementById ("toggle_advanced_search");
+    if (adv_search) { 
+        adv_search.addEventListener ('click', function () { 
+            var filters = document.querySelector (".search-filters")
+            if (filters.classList.contains ('hide')) {
+                filters.classList.remove ("hide");
+                this.innerText = 'Hide advanced filters';
+            } else {
+                filters.classList.add ("hide");
+                this.innerText = 'Advanced search';
+            }
+            
+        })
+    }
 })
